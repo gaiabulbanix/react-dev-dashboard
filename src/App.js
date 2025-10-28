256ce791650e2a6e53b56ce458c24e02c630b724
gaiabulbanix <frpcastillo@gmail.com>
2025-10-29 01:38:24 +0800

    feat: added Counter.js component, imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import myLogo from './assets/bunny1.png';
 import './Custom.css';
 import Greeting from './Greeting';
+import Counter from './Counter';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Hello, Gorby!</h1>
       <img src={myLogo} alt="Logo" width="150" />
       <Greeting name="Gorby" />
+      <Counter />
     </div>
   );
 }
