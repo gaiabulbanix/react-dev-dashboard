fb65fb905d56556242163fceb1dcdfc161223cb7
gaiabulbanix <frpcastillo@gmail.com>
2026-07-08 22:54:55 +0800

    fix: removed empty className inside TaskTracker.jsx, added button sizes to navigation buttons, made a main container and moved active features inside, added padding to it and removed padding from top-level div, changed header to look more seamless with the background

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,15 @@ import Button from './Button';
 export default function Navigation({ onFeatureChange }) {
     return (
         <div className="flex gap-6">
-            <Button onClick={() => onFeatureChange('taskTracker')}>Tasks</Button>
-            <Button onClick={() => onFeatureChange('fitnessTracker')}>Fitness</Button>
+            <Button
+                onClick={() => onFeatureChange('taskTracker')}
+                btnSize="sm"
+            >Tasks</Button>
+            <Button
+
+                onClick={() => onFeatureChange('fitnessTracker')}
+                btnSize="sm"
+            >Fitness</Button>
         </div>
     );
 }
--- src/features/task-tracker/TaskTracker.jsx
+++ src/features/task-tracker/TaskTracker.jsx
@@ -67,7 +67,7 @@ export default function TaskTracker({ className = '', }) {
     const handleClearCompleted = () => setTaskList(taskList.filter((task) => !task.complete));
 
     return (
-        <Panel className={`${className}`}>
+        <Panel>
             <h2>Task Tracker</h2>
             <div className="flex gap-2">
                 <TaskForm
@@ -102,6 +102,6 @@ export default function TaskTracker({ className = '', }) {
                     </Button>
                 </div>
             </div>
-        </Panel>
+        </Panel >
     );
 }
