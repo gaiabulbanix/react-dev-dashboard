4ed5aabadf5672ce938f6a574488f73196c38ccd
gaiabulbanix <frpcastillo@gmail.com>
2026-07-16 23:09:16 +0800

    feat: created and wired WorkoutForm.jsx component

--- src/features/fitness-tracker/FitnessTracker.jsx
+++ src/features/fitness-tracker/FitnessTracker.jsx
@@ -1,4 +1,5 @@
 import Panel from '../../components/Panel';
+import WorkoutForm from './WorkoutForm';
 
 import { useState } from 'react';
 
@@ -6,6 +7,7 @@ export default function FitnessTracker() {
     return (
         <Panel className="max-w-3xl">
             <h2>Fitness Tracker</h2>
+            <WorkoutForm />
         </Panel>
     );
 }
